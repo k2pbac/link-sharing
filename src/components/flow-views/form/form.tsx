ad66ea1bda486de0fd2151c0260fbf342c785732
k2pbac <krisbachan123@hotmail.com>
2024-07-10 11:43:06 -0400

    add header

--- src/components/flow-views/form/form.tsx
+++ src/components/flow-views/form/form.tsx
@@ -11,63 +11,70 @@ export default function Form() {
   const [passwordInput, setPasswordInput] = useState("");
 
   return (
-    <form className="flex flex-col gap-3 center-content form justify-center">
-      <h2 className="heading-m">Login</h2>
-      <p className="body-m">Add your details below to get back into the app</p>
-      <div>
-        <div className="mb-2 block">
-          <Label className="body-s" htmlFor="email" value="Email address" />
-        </div>
-        <div
-          className={`${emailInput} input-group-custom flex w-full border disabled:cursor-not-allowed disabled:opacity-50`}
-        >
-          <Image
-            src={"/assets/images/icon-email.svg"}
-            alt=""
-            width={16}
-            height={16}
-          />
-          <TextInput
-            className="w-full"
-            id="email"
-            type="email"
-            placeholder="e.g. alex@email.com"
-            required
-            onFocus={() => setEmailInput("focus-border")}
-            onBlur={() => setEmailInput("")}
-          />
-        </div>
+    <form className="flex flex-col center-content form justify-center">
+      <div className="title-container">
+        <img src="/assets/images/logo-devlinks-large.svg" alt="logo" />
       </div>
-      <div>
-        <div className="mb-2 block">
-          <Label className="body-s" htmlFor="password" value="Password" />
+      <div className="content-container flex flex-col gap-3 justify-center">
+        <h2 className="heading-m">Login</h2>
+        <p className="body-m">
+          Add your details below to get back into the app
+        </p>
+        <div>
+          <div className="mb-2 block">
+            <Label className="body-s" htmlFor="email" value="Email address" />
+          </div>
+          <div
+            className={`${emailInput} input-group-custom flex w-full border disabled:cursor-not-allowed disabled:opacity-50`}
+          >
+            <Image
+              src={"/assets/images/icon-email.svg"}
+              alt=""
+              width={16}
+              height={16}
+            />
+            <TextInput
+              className="w-full"
+              id="email"
+              type="email"
+              placeholder="e.g. alex@email.com"
+              required
+              onFocus={() => setEmailInput("focus-border")}
+              onBlur={() => setEmailInput("")}
+            />
+          </div>
         </div>
-        <div
-          className={`${passwordInput} input-group-custom flex w-full border disabled:cursor-not-allowed disabled:opacity-50`}
-        >
-          <Image
-            src={"/assets/images/icon-password.svg"}
-            alt=""
-            width={16}
-            height={16}
-          />
-          <TextInput
-            className="w-full"
-            id="password"
-            type="password"
-            placeholder="Enter your password"
-            required
-            onFocus={() => setPasswordInput("focus-border")}
-            onBlur={() => setPasswordInput("")}
-          />
+        <div>
+          <div className="mb-2 block">
+            <Label className="body-s" htmlFor="password" value="Password" />
+          </div>
+          <div
+            className={`${passwordInput} input-group-custom flex w-full border disabled:cursor-not-allowed disabled:opacity-50`}
+          >
+            <Image
+              src={"/assets/images/icon-password.svg"}
+              alt=""
+              width={16}
+              height={16}
+            />
+            <TextInput
+              className="w-full"
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+              required
+              onFocus={() => setPasswordInput("focus-border")}
+              onBlur={() => setPasswordInput("")}
+            />
+          </div>
         </div>
+        <Button className="mt-3" type="submit">
+          Submit
+        </Button>
+        <p className="text-center body-m pt-3">
+          Don’t have an account? <Link href="/signup">Create account</Link>
+        </p>
       </div>
-      <Button className="mt-3" type="submit">
-        Submit
-      </Button>
-      <p className="text-center body-m pt-3">
-        Don’t have an account? <Link href="/signup">Create account</Link>
-      </p>
     </form>
   );
 }
