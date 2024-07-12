d7211a73e2ca53e0756edfc60229e70cb5255ead
k2pbac <krisbachan123@hotmail.com>
2024-07-12 12:57:25 -0400

    add preview section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import CustomNav from "@/components/views/navbar/navbar";
+import Preview from "@/components/views/preview/preview";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="homepage min-h-screen">
       <CustomNav />
+      <div className="content">
+        <Preview />
+        <div></div>
+      </div>
     </main>
   );
 }
---
+++ src/components/views/preview/preview.tsx
@@ -0,0 +1,12 @@
+export default function Preview() {
+  return (
+    <div className="preview-container">
+      <div>
+        <img
+          src="/assets/images/illustration-phone-mockup.svg"
+          alt="link-preview"
+        />
+      </div>
+    </div>
+  );
+}
