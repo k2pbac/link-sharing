2800748ed94bed9f2e14b434a55b85d5de5cafe2
k2pbac <krisbachan123@hotmail.com>
2024-07-12 10:51:16 -0400

    updates on nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import CustomNav from "@/components/views/navbar/navbar";
 import Image from "next/image";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <CustomNav />
+    </main>
+  );
 }
---
+++ src/components/views/navbar/navbar.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Link from "next/link";
+import { Button, Navbar } from "flowbite-react";
+
+export default function CustomNav() {
+  return (
+    <Navbar className="navbar" fluid rounded>
+      <Navbar.Brand as={Link} href="https://flowbite-react.com">
+        <img
+          src="/assets/images/logo-devlinks-large.svg"
+          className="mr-3 h-6 sm:h-9"
+          alt="Devlinks Logo"
+        />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white"></span>
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+        <Button>Get started</Button>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <a href="#links" className="option active heading-s">
+          <img src="/assets/images/icon-links-header.svg" alt="link button" />
+          Links
+        </a>
+        <a href="#profile" className="option">
+          <img
+            src="/assets/images/icon-profile-details-header.svg"
+            alt="profile button"
+          />
+          Profile Details
+        </a>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
