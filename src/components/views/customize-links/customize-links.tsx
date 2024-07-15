e21113dd77cc52587b0dd0d7b5d81a83db607d03
k2pbac <krisbachan123@hotmail.com>
2024-07-15 07:56:50 -0400

    some mongo stuff

--- src/components/views/customize-links/customize-links.tsx
+++ src/components/views/customize-links/customize-links.tsx
@@ -3,13 +3,37 @@
 import { Button } from "flowbite-react";
 import { useState } from "react";
 import CustomLinkInput from "./custom-link-input";
+import axios from "axios";
+import router from "next/router";
+import data from "@/linksList.json";
 
 export default function CustomizeLinks() {
-  const [linksCount, setLinksCount] = useState(0);
+  const [links, setLinks] = useState<Object[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const handleAddNewLink = () => {
-    setLinksCount((prev) => prev + 1);
+    setLinks((prev) => [
+      ...prev,
+      { platform: data[prev.length + 1].item, url: "" },
+    ]);
   };
+
+  const addNewLinks = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/links", {
+        links,
+      });
+      router.push("/");
+    } catch (error: any) {
+      console.log("Login failed", error.message);
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="customize-container">
       <div className="header">
@@ -23,7 +47,7 @@ export default function CustomizeLinks() {
         <Button onClick={handleAddNewLink} className="add-link-button w-full">
           + Add new link
         </Button>
-        {linksCount == 0 ? (
+        {links.length == 0 ? (
           <div className="image-container">
             <>
               <img
@@ -41,15 +65,16 @@ export default function CustomizeLinks() {
             </>
           </div>
         ) : (
-          Array.from({ length: linksCount }, (_, key) => {
+          Array.from({ length: links.length }, (_, key) => {
             return <CustomLinkInput key={key} index={key} />;
           })
         )}
       </div>
       <div className="button-container">
         <Button
+          onClick={() => addNewLinks()}
           className={`save-button heading-s ${
-            linksCount == 0 ? "disabled" : ""
+            links.length == 0 ? "disabled" : ""
           }`}
         >
           Save
